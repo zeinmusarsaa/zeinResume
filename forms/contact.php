7a6c3b235c735fe5803c7b730e0b962cd54a3087
zeinmusarsaa <118689776+zeinmusarsaa@users.noreply.github.com>
2024-02-11 17:08:16 -0700

    all these things

---
+++ forms/contact.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect the data from the form
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $message = trim($_POST["message"]);
+
+    // Set your email address
+    $recipient = "zeinmusarsaa@gmail.com";
+
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers
+    $email_headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        echo "Thank You! Your message has been sent.";
+    } else {
+        echo "Oops! Something went wrong, and we couldn't send your message.";
+    }
+} else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
+
+
