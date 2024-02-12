862cc5ae1ea495ed643e65634e02ae4f7d667bda
zeinmusarsaa <118689776+zeinmusarsaa@users.noreply.github.com>
2024-02-11 18:33:26 -0700

    a

--- forms/contact.php
+++ forms/contact.php
@@ -1,33 +1,42 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect the data from the form
-    $name = strip_tags(trim($_POST["name"]));
-    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $subject = strip_tags(trim($_POST["subject"]));
-    $message = trim($_POST["message"]);
 
-    // Set your email address
-    $recipient = "zeinmusarsaa@gmail.com";
+require '../PHPMailer-master/src/Exception.php';
+require '../PHPMailer-master/src/PHPMailer.php';
+require '../PHPMailer-master/src/SMTP.php';
 
-    // Build the email content
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    // Build the email headers
-    $email_headers = "From: $name <$email>";
+$mail = new PHPMailer(true); // Passing `true` enables exceptions
 
-    // Send the email
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
-        echo "Thank You! Your message has been sent.";
-    } else {
-        echo "Oops! Something went wrong, and we couldn't send your message.";
-    }
-} else {
-    // Not a POST request, set a 403 (forbidden) response code.
-    http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                      // Enable verbose debug output
+    $mail->isSMTP();                                           // Set mailer to use SMTP
+    $mail->Host       = 'smtp.example.com';                    // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                  // Enable SMTP authentication
+    $mail->Username   = 'zeinmusarsaa@gmail.com';                    // SMTP username
+    $mail->Password   = 'zeinZEIN13122001!';                        // SMTP password
+    $mail->SMTPSecure = 'tls';                                 // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                   // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('your@example.com', 'Your Name');
+    $mail->addAddress('recipient@example.com', 'Recipient Name');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                       // Set email format to HTML
+    $mail->Subject = 'Subject Here';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+    $mail->send();
+    echo 'Thank You! Your message has been sent.';
+} catch (Exception $e) {
+    echo "Oops! Something went wrong and we couldn't send your message. Error: {$mail->ErrorInfo}";
 }
+
+
 ?>
+
 
 
